00c1e4117cedade5337d8078ef034eb633d064f6
nikolay_tikhonov <ntikhonov@gridgain.com>
2015-03-27 12:24:22 +0300

    Fixed ignite-driver-aws scripts and Dockerfile.

--- ignite-driver-aws/benchmark-upload.sh
+++ ignite-driver-aws/benchmark-upload.sh
@@ -18,7 +18,7 @@ done
 rename 's/results/ignite-results/' results*
 
 # If remote dir contains benchmark result then will generate compare graph.
-cnt_bench=$(ls /mnt/*-results-* 2>/dev/null | wc -l)
+cnt_bench=$(ls /mnt/*-results-*.tar.gz 2>/dev/null | wc -l)
 
 if [ ${cnt_bench} != 0 ]; then
     echo "Remote directory contains another benchmark results.";
@@ -27,9 +27,16 @@ if [ ${cnt_bench} != 0 ]; then
 
     echo "Start download result from storage."
 
-    cp -R /mnt/*-results-* ./temp_comp
+    cp /mnt/*-results-*.tar.gz ./temp_comp
 
     echo "Results downloaded."
+
+    for filename in temp_comp/*.tar.gz
+    do
+      tar zxf $filename
+    done
+
+    rm -rf temp_comp/*.tar.gz
 
     cp -R ./*-results-* ./temp_comp
 
@@ -37,17 +44,33 @@ if [ ${cnt_bench} != 0 ]; then
 
     bin/jfreechart-graph-gen.sh -gm COMPARISON -i ${lsArra[@]}
 
+    for dir in results-comparison-*
+    do
+        base=$(basename "$dir")
+        tar -czf "${base}$(date -d "today" +"%H%M").tar.gz" "$dir"
+    done
+
     echo "Start upload comparison to storage."
 
-    mv results-comparison-* /mnt
+    mv results-comparison-*.tar.gz /mnt
 
     echo "Comparison results uploaded."
+
+    rm -rf results-comparison-*
 fi;
+
+for dir in ignite-results-*
+do
+    base=$(basename "$dir")
+    tar -czf "${base}.tar.gz" "$dir"
+done
 
 echo "Start upload benchmark results to storage."
 
-mv ignite-results-* /mnt
+mv ignite-results-*.tar.gz /mnt
 
 echo "Results uploaded."
+
+rm -rf ignite-results-*
 
 rm -rf temp_comp
