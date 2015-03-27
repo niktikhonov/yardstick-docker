074fae699b4a52759071aeb0f2f70e9aba76c4da
nikolay_tikhonov <ntikhonov@gridgain.com>
2015-03-27 17:44:57 +0300

    Added scripts for AIM.

---
+++ ignite-driver-aws/start-driver-server.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+    echo "Illegal number of parameters"
+    echo "Usage: start-benchmark-driver.sh AWS_ACCESS_KEY AWS_SECRET_KEY [S3_BUCKET]"
+
+    exit 1
+fi
+
+sudo -S docker pull ntikhonov/yardstick-ignite-driver-aws
+
+if [ -z "$3" ]; then
+    sudo -S docker run -d --net=host --privileged --restart=always -e AWS_ACCESS_KEY=$1 -e AWS_SECRET_KEY=$2 \
+        ntikhonov/yardstick-ignite-driver-aws
+else
+    sudo -S docker run -d --net=host --privileged --restart=always -e AWS_ACCESS_KEY=$1 -e AWS_SECRET_KEY=$2 \
+        -e ES3_BUCKET=$3 ntikhonov/yardstick-ignite-driver-aws
+fi
